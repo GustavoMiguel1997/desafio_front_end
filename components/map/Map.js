7e720a6795e254dd003fc30cf1089f4aaa8fa217
gustavo.silva <gustavo.silva@geofusion.com.br>
2019-10-16 19:29:30 -0300

    Criando funcionalidade na filtragem de lojas no mapa

--- components/map/Map.js
+++ components/map/Map.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Map, TileLayer, Marker } from "react-leaflet";
 import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import "../../node_modules/leaflet/dist/leaflet.css";
 import "./Map.css";
 
-export default function Mao() {
+export default function MapCreate({ storesList, userText, userRevenue }) {
+  userText = userText ? userText.value : "";
+  userRevenue = userRevenue ? userRevenue.value : 15000;
+
+  const blueIcon = L.icon({
+    iconUrl: require("../../resources/images/marker-blue.png"),
+    iconSize: [15, 15]
+  });
+
+  const redIcon = L.icon({
+    iconUrl: require("../../resources/images/marker-red.png"),
+    iconSize: [15, 15]
+  });
+
   const position = [-23.6, -46.67];
   return (
     <div className="map-style">
@@ -13,7 +26,32 @@ export default function Mao() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        {}
+        {storesList.map(({ latitude, longitude, revenue, name }) => {
+          userRevenue = userRevenue ? userRevenue : 15000;
+          if (
+            name.toLowerCase().includes(userText.toLowerCase()) &&
+            revenue > userRevenue
+          ) {
+            return (
+              <Marker
+                position={[latitude, longitude]}
+                icon={blueIcon}
+                key={revenue}
+              />
+            );
+          } else if (
+            name.toLowerCase().includes(userText.toLowerCase()) &&
+            revenue < userRevenue
+          ) {
+            return (
+              <Marker
+                position={[latitude, longitude]}
+                icon={redIcon}
+                key={revenue}
+              />
+            );
+          }
+        })}
       </Map>
     </div>
   );
